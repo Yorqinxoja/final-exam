93b44167d6f64df795018170dceff1589657f4de
Yorqinxoja <yooorknxoja@gmail.com>
2024-10-09 20:08:41 +0500

    w

--- src/components/brand/Brand.tsx
+++ src/components/brand/Brand.tsx
@@ -2,35 +2,18 @@ import by from "../../assets/by.png";
 import ave from "../../assets/ave.png";
 import jos from "../../assets/jos.png";
 import skin from "../../assets/skin.png";
-import mie from "../../assets/mie.png";
-import cera from "../../assets/cera.png";
-import hair from "../../assets/hair.png";
 import "./Brand.css";
 import { useRef } from "react";
-import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Brand = () => {
   const sliderRef = useRef(null);
 
-  const scrollLeft = () => {
-    if (sliderRef.current) {
-      sliderRef.current.scrollLeft -= 300;
-    }
-  };
 
-  const scrollRight = () => {
-    if (sliderRef.current) {
-      sliderRef.current.scrollLeft += 300;
-    }
-  };
 
   return (
     <div className="brand-container">
       <h1 className="brand-title">SHOP by BRAND</h1>
 
-      <button className="slider-btn left" onClick={scrollLeft}>
-        <FiChevronLeft />
-      </button>
 
       <div className="brand-slider" ref={sliderRef}>
         {[
@@ -38,21 +21,7 @@ const Brand = () => {
           { img: ave, name: "AVENE" },
           { img: jos, name: "BEAUTY OF JOSEON" },
           { img: skin, name: "SKIN1004" },
-          { img: mie, name: "MIELLE ORGANICS" },
-          { img: cera, name: "CERAVE" },
-          { img: hair, name: "HAIRMAX" },
-          { img: by, name: "BY BEAUTY BAY" },
-          { img: ave, name: "AVENE" },
-          { img: jos, name: "BEAUTY OF JOSEON" },
-          { img: skin, name: "SKIN1004" },
-          { img: mie, name: "MIELLE ORGANICS" },
-          { img: cera, name: "CERAVE" },
-          { img: hair, name: "HAIRMAX" },
-          { img: by, name: "BY BEAUTY BAY" },
-          { img: ave, name: "AVENE" },
-          { img: jos, name: "BEAUTY OF JOSEON" },
-          { img: skin, name: "SKIN1004" },
-          { img: mie, name: "MIELLE ORGANICS" },
+
         ].map((brand, index) => (
           <div key={index} className="brand-card">
             <img src={brand.img} alt={brand.name} className="brand-image" />
@@ -61,9 +30,6 @@ const Brand = () => {
         ))}
       </div>
 
-      <button className="slider-btn right" onClick={scrollRight}>
-        <FiChevronRight />
-      </button>
     </div>
   );
 };
