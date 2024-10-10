249a70e534eae8fdec51fe4f0fe68e41ed30ed98
Yorqinxoja <yooorknxoja@gmail.com>
2024-10-10 23:40:20 +0500

    change loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,9 @@
-// import Home from "./routes/home/Home"
-// import { Route, Routes } from "react-router-dom"
-// import Cart from "./routes/cart/Cart"
-// import Like from "./routes/like/Like"
-// import Deatels from "./routes/details/Deatels"
-
-// function App() {
-
-//   return (
-//     <>
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-//         <Route path="/cart" element={<Cart />} />
-//         <Route path="/like" element={<Like />} />
-//         <Route path='/details/:productId' element={<Deatels />} />
-//         <Route path="*" element={<Home />} />
-//       </Routes>
-//     </>
-//   )
-// }
-
-// export default App
-
-
 import Home from "./routes/home/Home";
 import { Route, Routes } from "react-router-dom";
 import Cart from "./routes/cart/Cart";
 import Like from "./routes/like/Like";
 import Deatels from "./routes/details/Deatels";
-import MainComponent from "./components/MainComponent"; // Adjust the path based on your structure
+import MainComponent from "./components/loading/MainComponent";
 
 function App() {
   return (
